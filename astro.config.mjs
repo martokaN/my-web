a972542adce5d758fadeced4503270659e315163
martokaN <marek.sladek10@seznam.cz>
2026-04-13 11:37:15 +0200

    No adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,27 @@ import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  integrations: [icon()],
+	integrations: [icon()],
 
-  image: {
-      responsiveStyles: true,
-      layout: "constrained",
+	image: {
+		responsiveStyles: true,
+		layout: "constrained",
 	},
 
-  fonts: [
-      {
-          provider: fontProviders.google(),
-          name: "Plus Jakarta Sans",
-          cssVariable: "--font-body",
-          weights: [400, 700],
-          subsets: ["latin", "latin-ext"],
-      },
-      {
-          provider: fontProviders.google(),
-          name: "DM Sans",
-          cssVariable: "--font-heading",
-          weights: [400, 700],
-          subsets: ["latin", "latin-ext"],
-      },
+	fonts: [
+		{
+			provider: fontProviders.google(),
+			name: "Plus Jakarta Sans",
+			cssVariable: "--font-body",
+			weights: [400, 700],
+			subsets: ["latin", "latin-ext"],
+		},
+		{
+			provider: fontProviders.google(),
+			name: "DM Sans",
+			cssVariable: "--font-heading",
+			weights: [400, 700],
+			subsets: ["latin", "latin-ext"],
+		},
 	],
-
-  adapter: cloudflare(),
 });
